513cfa6e6c55a5a68e28c4ac99a2d41322529f65
Rafael Reali <rafaelreali2011@hotmail.com>
2025-04-28 21:39:15 -0300

    Mudanças na seção de inicio e nav bar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 window.addEventListener("DOMContentLoaded", () => {
     lucide.createIcons();
   });
+  let lastScrollTop = 0;
+  const nav = document.querySelector('nav');
+  
+  window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+     
+      nav.style.top = "-100px";
+    } else {
+      
+      nav.style.top = "10px"; 
+    }
+    
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; //
+  });
+
