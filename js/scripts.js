e9516dd67b4b76a5c3c0f217ccaaf36db7bfeb14
rafaelreali <rafaelreali2011@hotmail.com>
2026-02-17 19:10:33 -0300

    projeto 3 adicionado 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
 window.addEventListener("DOMContentLoaded", () => {
+  
+  if (window.lucide) {
     lucide.createIcons();
-  });
+  }
+
+  
   let lastScrollTop = 0;
   const nav = document.querySelector('nav');
-  
+
   window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    
+
     if (scrollTop > lastScrollTop) {
-     
-      nav.style.top = "-100px";
+      nav.style.top = "-100px"; 
     } else {
-      
-      nav.style.top = "10px"; 
+      nav.style.top = "10px";   
     }
-    
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; //
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
   });
 
-
-
-
  
+  const habilidades = document.querySelectorAll('.habilidade');
+  const descricaoBox = document.getElementById("descricao-habilidade");
+  const textoPadrao = "Passe o mouse sobre um ícone para ver a descrição.";
+
+  habilidades.forEach(element => {
+    element.addEventListener('mouseout', () => {
+      descricaoBox.innerText = textoPadrao;
+    });
+  });
+});
+
+
+function mostrarDescricao(texto) {
+  const descricao = document.getElementById("descricao-habilidade");
+  descricao.innerText = texto;
+}
